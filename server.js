38308b581c89bf43efe929a267e5cec9ccc844af
christine <christinec@missionreadyhq.com>
2025-08-21 17:38:17 +1200

    feat: added cors, express.. added comments

--- server.js
+++ server.js
@@ -2,30 +2,35 @@
 import express from "express";
 import dotenv from "dotenv";
 import { getPrediction } from "./prediction.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 app.post("/prediction", async (req, res) => {
   const { imgURL } = req.body;
+  console.log("Received image URL:", imgURL);
 
   // if it's not valid image url, return error msg
   if (!imgURL) {
     return res
       .status(400)
-      .json({ error: "Missing image URL, please try again!" });
+      .json({ error: "❌ Missing image URL, please try again!" });
   }
 
-  //  return prediction & handling error
+  //  send image url to azure & return prediction & handling error
   try {
-    const prediction = await getPrediction(imgURL);
-    res.json({ prediction });
+    const response = await getPrediction(imgURL);
+    res.json({ response });
+    console.log("Prediction result", response);
   } catch (error) {
-    console.error("oh no");
+    console.error("oh no, something went wrong. Please try again.");
     res.status(500).json({ error: error.message });
+    console.error("prediction error", error.nessage);
   }
 });
 
@@ -33,25 +38,5 @@ app.post("/prediction", async (req, res) => {
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`prediction app listening on port http://localhost:${PORT}`);
+  console.log(`server is listening on port http://localhost:${PORT}`);
 });
-
-// const PredictionApi = require("@azure/cognitiveservices-customvision-prediction");
-// const msRest = require("@azure/ms-rest-js");
-
-// // retrieve environment variables
-// const predictionKey = process.env["VISION_PREDICTION_KEY"];
-// const predictionResourceId = process.env["VISION_PREDICTION_RESOURCE_ID"];
-// const predictionEndpoint = process.env["VISION_PREDICTION_ENDPOINT"];
-
-// const publishIterationName = "classifyModel";
-
-// // Azure Custom Vision client
-// const predictor_credentials = new msRest.ApiKeyCredentials({
-//   inHeader: { "Prediction-key": predictionKey },
-// });
-
-// const predictor = new PredictionApi.PredictionAPIClient(
-//   predictor_credentials,
-//   predictionEndpoint
-// );
